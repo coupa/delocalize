cde426d43fad2124ae8decc8fdb4c34ba0cbdbb0
Chris Fung <aergonaut@gmail.com>
2021-04-12 12:18:13 -0700

    add extension file for Rails 6.1

--- lib/delocalize/rails_ext/active_record.rb
+++ lib/delocalize/rails_ext/active_record.rb
@@ -1,4 +1,6 @@
-if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('5.2')
+if Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('6.1')
+  require 'delocalize/rails_ext/active_record_rails61'
+elsif Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('5.2')
   require 'delocalize/rails_ext/active_record_rails52'
 elsif Gem::Version.new(ActionPack::VERSION::STRING) >= Gem::Version.new('4.2')
   require 'delocalize/rails_ext/active_record_rails42'
